360a1d06fef49e5c9d051691373e8689e99c8c25
Evgeny Kniazev <kniazev-evg@ya.ru>
2020-10-06 23:09:00 +0300

    upd: mobile optimisations

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
     v-layout(column  style="background-color: #f5f6f8;")
-        v-parallax(row, :src="require('../assets/img/' + imgNum)", :height="isMobile ? '560' : '790'", id="parallax1" :style="isMobile ? 'height: 680px' : 'height: 840px'")
+        v-parallax(row, :src="require('../assets/img/' + imgNum)", :height="isMobile ? '660' : '790'", id="parallax1" :style="isMobile ? 'height: 700px' : 'height: 840px'")
             v-card(flat, style="background: rgba(0, 0, 0, 0); width: 90%; margin-left: auto; margin-right: auto; height: 100%;")
                 v-layout(style="text-align: center; bottom: 25%; position: absolute; left: 0; right: 0;" wrap)
                     v-spacer
